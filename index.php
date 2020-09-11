cbf541d1cf5bb38cbf2b08d110d066763864ae28
Miguel <39063612+miguelangelpatino@users.noreply.github.com>
2020-09-11 00:12:32 -0500

    login

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-este es el archivo index - hice un cambio
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app/assets/css/semantic.min.css">
+    <link rel="stylesheet" href="app/assets/css/style.css">
+    <title>Login</title>
+</head>
+<body>
+    <main id="login">
+        <section id="marca">
+            <h1>UniExpress</h1>
+            <p class="italic12">La mejor manera de mercar</p>
+        </section>
+        <section id="info">
+            <h2>Iniciar sesión</h2>
+            <form id="form-login" action="">
+                <select id="select" class="input-formulario" name=""  id="">
+                    <option value="">Selecciona</option>
+                    <option value="">Admin</option>
+                    <option value="">Agente</option>
+                </select>
+                <input type="text" class="input-formulario" name="" placeholder="Usuario" id="2">
+                <input type="password" class="input-formulario" placeholder="Contraseña" name="" id="">
+                <input type="button" class="btn-degradado" value="Ingresar">
+            </form>
+        </section>
+    </main>
+</body>
+</html>

f2e538802ca21b9c40cec4fe05ab5e04d6aafb1c
gitjaleman <javiersettex@gmail.com>
2020-09-18 23:52:55 -0500

    prueba paralela

--- app/views/index.php
+++ app/views/index.php
@@ -1,1 +1,3 @@
-soy la vista principal (index) <br>
+<section>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid earum tenetur est pariatur quisquam quos, ipsa placeat, qui debitis accusamus consectetur repudiandae, veniam vel facilis officiis quidem praesentium dignissimos adipisci!git 
+</section>
